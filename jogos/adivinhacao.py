7ff682fdd5cfbe20dc9d6ffbcb2a44709ce6087b
jcfbernardo <joaoc.bernardo12@gmail.com>
2022-11-30 21:10:26 -0300

    Implementação de menu de jogos

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,31 +1,32 @@
-print('***********************')
-print('* Jogo da Adivinhação *')
-print('***********************')
+def jogar():
+    print('***********************')
+    print('* Jogo da Adivinhação *')
+    print('***********************')
 
-numero_secreto = 10
+    numero_secreto = 10
 
-tentativas = 3
+    tentativas = 3
 
-while tentativas > 0:
-    chute = int(input('Digite um número: '))
-    print('Você digitou {}\n'.format(chute))
+    while tentativas > 0:
+        chute = int(input('Digite um número: '))
+        print('Você digitou {}\n'.format(chute))
 
-    if chute > numero_secreto:
-        print('O número digitado é maior que o número secreto')
-        if tentativas == 1:
-            tentativas = tentativas - 1
-            print('\nPERDEU!\nVocê não tem mais tentativas')
+        if chute > numero_secreto:
+            print('O número digitado é maior que o número secreto')
+            if tentativas == 1:
+                tentativas = tentativas - 1
+                print('\nPERDEU!\nVocê não tem mais tentativas')
+            else:
+                tentativas = tentativas - 1
+                print('Você agora tem {} tentativas'.format(tentativas))
+        elif chute < numero_secreto:
+            print('O número digitado é menor que o número secreto')
+            if tentativas == 1:
+                tentativas = tentativas - 1
+                print('\nPERDEU!\nVocê não tem mais tentativas')
+            else:
+                tentativas = tentativas - 1
+                print('Você agora tem {} tentativas'.format(tentativas))
         else:
-            tentativas = tentativas - 1
-            print('Você agora tem {} tentativas'.format(tentativas))
-    elif chute < numero_secreto:
-        print('O número digitado é menor que o número secreto')
-        if tentativas == 1:
-            tentativas = tentativas - 1
-            print('\nPERDEU!\nVocê não tem mais tentativas')
-        else:
-            tentativas = tentativas - 1
-            print('Você agora tem {} tentativas'.format(tentativas))
-    else:
-        print('\nPARABÉNS! VOCÊ ACERTOU!\nO número secreto é {}'.format(numero_secreto))
-        break
+            print('\nPARABÉNS! VOCÊ ACERTOU!\nO número secreto é {}'.format(numero_secreto))
+            break
--- jogos/forca.py
+++ jogos/forca.py
@@ -1,36 +1,37 @@
-print('*****************')
-print('* Jogo da Forca *')
-print('*****************')
+def jogar():
+    print('*****************')
+    print('* Jogo da Forca *')
+    print('*****************')
 
-palavra_secreta = 'futebol'
-segredo = []
+    palavra_secreta = 'futebol'
+    segredo = []
 
-for palavra in palavra_secreta:
-    segredo.append('_')
+    for palavra in palavra_secreta:
+        segredo.append('_')
 
-erros = 0
-acertou = False
-enforcou = erros == 6
-
-
-while(not acertou and not enforcou):
-    chute = input('Qual letra? ')
-
-    if (chute in palavra_secreta):
-        posicao = 0
-        for letra in palavra_secreta:
-            if (chute == letra):
-                segredo[posicao] = letra
-            posicao += 1
-    else:
-        erros += 1
-
+    erros = 0
+    acertou = False
     enforcou = erros == 6
-    acertou = '_' not in segredo
-    print(segredo)
 
-if(acertou):
-    print('Você	ganhou!!')
-else:
-    print('Você	perdeu!!')
-print('Fim	do	jogo')
+
+    while(not acertou and not enforcou):
+        chute = input('Qual letra? ')
+
+        if (chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    segredo[posicao] = letra
+                posicao += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = '_' not in segredo
+        print(segredo)
+
+    if(acertou):
+        print('Você	ganhou!!')
+    else:
+        print('Você	perdeu!!')
+    print('Fim	do	jogo')
